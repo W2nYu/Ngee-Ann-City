30dfa5d5ecac2cb112a4dee6e4f9be446a1409b6
Shuqri Shaifuddin <s10227932@connect.np.edu.sg>
2022-12-05 21:51:57 +0800

    added new_game() function

--- Ngee Ann City.py
+++ Ngee Ann City.py
@@ -1,4 +1,16 @@
-#Test Lol
 
-for i in range(6):
-    print("Test test 123")
+def new_game():
+    print('test new_game')
+    pass
+
+
+if __name__ == "__main__":
+    while True:
+        # Global variable initialisation and setting
+        pass
+
+        # Print statement to display the menu and prompt user for choice
+        new_game()
+        break
+
+        # If statements to execute functions based on choice
