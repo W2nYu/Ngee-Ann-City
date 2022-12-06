a784079b9410dbddf0ef1c0038c7171b325511e8
Shuqri Shaifuddin <s10227932@connect.np.edu.sg>
2022-12-06 22:58:21 +0800

    minor changes to new_game() NACSDD-5

--- Ngee Ann City.py
+++ Ngee Ann City.py
@@ -1,15 +1,33 @@
+def new_game(coins):
+    while coins != 0:
+        show_grid()
+        print('1. Build building')
+        print('2. See current score')
+        print('3. Exit')
 
-def new_game():
-    print('test new_game')
-    # addition to this thing
+        menu_choice = input("Choice: ")
+
+        if menu_choice == "1":
+            build_building()
+        elif menu_choice == "2":
+
+
+
+
+def build_building():
     pass
 
+def show_grid():
+    pass
 
 if __name__ == "__main__":
     while True:
         # Global variable initialisation and setting
-        pass
+        grid_size = 20
+        grid = [["   " for col in range(grid_size)] for row in range(grid_size)]
+        coins = 16
 
+        print()
         # Print statement to display the menu and prompt user for choice
         new_game()
         break
